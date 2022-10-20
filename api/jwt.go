a9fa3ecd87f4c24da95fa0edc0621b487af01e55
springeye <henjue@gmail.com>
2022-10-20 18:14:47 +0800

    update

--- api/jwt.go
+++ api/jwt.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/springeye/droidjs-server/db"
+	"github.com/springeye/droidjs-server/proto"
 	"log"
 	"time"
 )
@@ -10,11 +12,6 @@ import (
 	jwt "github.com/appleboy/gin-jwt/v2"
 )
 
-type login struct {
-	Username string `form:"username" json:"username" binding:"required"`
-	Password string `form:"password" json:"password" binding:"required"`
-}
-
 var identityKey = "id"
 
 func helloHandler(c *gin.Context) {
@@ -22,16 +19,9 @@ func helloHandler(c *gin.Context) {
 	user, _ := c.Get(identityKey)
 	c.JSON(200, gin.H{
 		"userID":   claims[identityKey],
-		"userName": user.(*User).UserName,
+		"userName": user.(*db.User).Username,
 		"text":     "Hello World.",
 	})
-}
-
-// User demo
-type User struct {
-	UserName  string
-	FirstName string
-	LastName  string
 }
 
 func SetupJwt(r *gin.Engine) *gin.RouterGroup {
@@ -43,43 +33,44 @@ func SetupJwt(r *gin.Engine) *gin.RouterGroup {
 		MaxRefresh:  time.Hour,
 		IdentityKey: identityKey,
 		PayloadFunc: func(data interface{}) jwt.MapClaims {
-			if v, ok := data.(*User); ok {
+			if v, ok := data.(*db.User); ok {
 				return jwt.MapClaims{
-					identityKey: v.UserName,
+					identityKey: v.ID,
 				}
 			}
 			return jwt.MapClaims{}
 		},
+		//带着token的请求会经过该接口进行解析token和验证
 		IdentityHandler: func(c *gin.Context) interface{} {
 			claims := jwt.ExtractClaims(c)
-			return &User{
-				UserName: claims[identityKey].(string),
+			var user db.User
+			if err := db.GetDB().Where("id = ?", claims[identityKey].(string)).First(&user); err == nil {
+				return &user
 			}
+			return nil
 		},
+		//登录接口，会验证信息和权限，并返回结果
 		Authenticator: func(c *gin.Context) (interface{}, error) {
-			var loginVals login
+			var loginVals proto.LoginRequest
 			if err := c.ShouldBind(&loginVals); err != nil {
 				return "", jwt.ErrMissingLoginValues
 			}
-			userID := loginVals.Username
+			username := loginVals.Username
 			password := loginVals.Password
-
-			if (userID == "admin" && password == "admin") || (userID == "test" && password == "test") {
-				return &User{
-					UserName:  userID,
-					LastName:  "Bo-Yi",
-					FirstName: "Wu",
-				}, nil
+			tx := db.GetDB()
+			var user db.User
+			err := tx.Where("username = ?", username).Find(&user).Error
+			if err == nil {
+				if user.Password == md5V(password+user.Salt) {
+					return &user, nil
+				}
 			}
 
 			return nil, jwt.ErrFailedAuthentication
 		},
+		////当用户通过token请求受限接口时，会经过这段逻辑
 		Authorizator: func(data interface{}, c *gin.Context) bool {
-			if v, ok := data.(*User); ok && v.UserName == "admin" {
-				return true
-			}
-
-			return false
+			return true
 		},
 		Unauthorized: func(c *gin.Context, code int, message string) {
 			c.JSON(code, gin.H{
@@ -87,14 +78,6 @@ func SetupJwt(r *gin.Engine) *gin.RouterGroup {
 				"message": message,
 			})
 		},
-		// TokenLookup is a string in the form of "<source>:<name>" that is used
-		// to extract token from the request.
-		// Optional. Default value "header:Authorization".
-		// Possible values:
-		// - "header:<name>"
-		// - "query:<name>"
-		// - "cookie:<name>"
-		// - "param:<name>"
 		TokenLookup: "header: Authorization, query: token, cookie: jwt",
 		// TokenLookup: "query:token",
 		// TokenLookup: "cookie:token",
